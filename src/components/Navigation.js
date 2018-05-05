912a5c4fe8bef8d6ded25226524ca17e096bd558
Ariel Fuggini <arielfuggini@gmail.com>
2018-05-05 15:29:41 -0300

    Added link to Github

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,13 +1,21 @@
 import React from 'react'
 import {
   Navbar,
-  NavbarBrand
+  NavbarBrand,
+  Nav,
+  NavItem,
+  NavLink
 } from 'reactstrap'
 
 const Navigation = props => {
   return (
     <Navbar className='mb-5' color='light' light expand='md'>
       <NavbarBrand href='/'>{props.title}</NavbarBrand>
+      <Nav className='ml-auto' navbar>
+        <NavItem>
+          <NavLink href='https://github.com/afuggini/react-foursquare' target='_blank'>Github</NavLink>
+        </NavItem>
+      </Nav>
     </Navbar>
   )
 }
